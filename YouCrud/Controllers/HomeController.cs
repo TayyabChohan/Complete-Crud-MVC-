d28005d3c581fdafcb21364dfca6af980b7d1be7
TayyabChohan <33210220+TayyabChohan@users.noreply.github.com>
2019-01-31 12:40:39 +0500

    Complete Crud

--- YouCrud/Controllers/HomeController.cs
+++ YouCrud/Controllers/HomeController.cs
@@ -3,28 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using YouCrud.Models;
 
 namespace YouCrud.Controllers
 {
     public class HomeController : Controller
     {
+        StudentInfoEntities db = new StudentInfoEntities();
         public ActionResult Index()
         {
+            var list = db.EmployeeInfoes.ToList();
+            return View(list);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            
+     
             return View();
         }
-
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Create(EmployeeInfo tb)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            db.EmployeeInfoes.Add(tb);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
-
-        public ActionResult Contact()
+        [HttpGet]
+        public ActionResult Edit(int id)
         {
-            ViewBag.Message = "Your contact page.";
 
-            return View();
+           EmployeeInfo emp =db.EmployeeInfoes.Find(id);
+            return View(emp);
+        }
+        [HttpPost]
+        public ActionResult Edit( EmployeeInfo emp , int id)
+        {
+            EmployeeInfo employeeInfo = db.EmployeeInfoes.SingleOrDefault(m => m.EmpoyeeID == id);
+            employeeInfo.EmpoyeeID = emp.EmpoyeeID;
+            employeeInfo.Name = emp.Name;
+            employeeInfo.Contact = emp.Contact;
+            employeeInfo.About = emp.About;
+            employeeInfo.Gender = emp.Gender;
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        public ActionResult Details(int id)
+        {
+            EmployeeInfo employeeInfo = db.EmployeeInfoes.Find(id);
+            if (employeeInfo == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(employeeInfo);
+        }
+        public ActionResult Delete(int id)
+        {
+            EmployeeInfo employeeInfo = db.EmployeeInfoes.Find(id);
+            db.EmployeeInfoes.Remove(employeeInfo);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
